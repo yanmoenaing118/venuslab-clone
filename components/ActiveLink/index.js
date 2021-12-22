527a92262aca4764e19e4c7a83eb86212d978374
yanmoenaing118 <yan.webdev.1234@gmail.com>
2021-12-22 09:05:43 +0630

    fix error remove onClick on link

--- components/ActiveLink/index.js
+++ components/ActiveLink/index.js
@@ -2,15 +2,9 @@ import Link from "next/link";
 import { withRouter } from "next/router";
 
 function ActiveLink({ router, href, children }) {
-  const onClick = (e) => {
-    e.preventDefault();
-    router.push(href);
-  };
-
   return (
     <Link href={href}>
       <a
-        onClick={onClick}
         style={{
           textDecoration: "none",
           color: router.pathname == href ? "rgba(var(--primary))" : "#fff",
